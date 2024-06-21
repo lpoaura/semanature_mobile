94ab17fc6ba4ef9221f19c5f8626483a4b5d850b
Niels Kristen <nkristen@free.fr>
2024-06-21 06:51:02 +0200

    Syntax error fix

--- app/pages/components/CodeGame/CodeGame.component.js
+++ app/pages/components/CodeGame/CodeGame.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, BackHandler, TextInput, ScrollView, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Audio } from 'expo-av'; // Import Audio from expo-av for sound handling
@@ -7,11 +7,12 @@ import TopBarre from '../../../components/TopBarre/TopBarre.component';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import MainTitle from './../../../components/MainTitle/MainTitle.component';
 import NormalizeStrings from './../../../utils/normalizeStrings';
-import { getParcoursContents } from "../../../utils/queries";
+import {getParcoursContents} from "../../../utils/queries";
 
 class CodeGame extends Component {
     constructor(props) {
         super(props);
+        // par défaut on utilise blockConfirm pour empêcher de passer à la page suivante
         this.state = {
             code: this.props.currentGame.code,
             blockConfirm: true,
@@ -26,16 +27,23 @@ class CodeGame extends Component {
         BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
         this.loadSound();
     }
-
+    
     componentWillUnmount() {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
         this.unloadSound();
     }
 
+    /**
+     * Permet de bloquer le retour arrière
+     * @returns 
+     */
     handleBackButtonClick() {
         return true;
     }
 
+    /**
+     * Permet d'éviter le spam click
+     */
     handleConfirmClicked = () => {
         if (!this.state.confirmClicked) {
             this.setState({ confirmClicked: true });
@@ -72,49 +80,54 @@ class CodeGame extends Component {
     }
 
     render() {
-        const { texte, etape_max, n_etape, nom, image_url } = this.props.currentGame;
-        const topBarreName = etape_max ? `Étape : ${n_etape}/${etape_max}` : '';
-        const icone = require('./../../../assets/code_paysage_icone.png');
-
-        return (
-            <SafeAreaView style={styles.outsideSafeArea}>
-                <TopBarre name={topBarreName} />
-                <View style={styles.globalContainer}>
-                    <ScrollView contentContainerStyle={styles.scrollViewContainer} style={styles.scrollView}>
-                        <View style={styles.card}>
-                            <MainTitle title={nom} icone={icone} />
-                            {image_url !== '' && <Image source={{ uri: image_url }} style={styles.areaImage} />}
-                            <Text style={styles.description}>{texte}</Text>
-                            <TextInput style={styles.inputTextField} onChangeText={this.handleChange} editable={true} placeholder="CODE" />
-                        </View>
-                        <View style={styles.rightAlign}>
-                            <TouchableOpacity
-                                style={styles.bouton}
-                                disabled={this.state.confirmClicked}
-                                onPress={() => {
-                                    if (NormalizeStrings(this.state.code) === NormalizeStrings(this.state.input)) {
-                                        this.handleConfirmClicked();
-                                        this.props.navigation.navigate("GamePage", { parcoursInfo: this.props.parcoursInfo, parcours: this.props.parcours });
-                                    } else {
-                                        Alert.alert("Mauvais code !");
-                                    }
-                                }}
-                            >
-                                <Text style={styles.boutonText}>Valider</Text>
-                            </TouchableOpacity>
-                            {this.state.isSoundLoaded && (
-                                <TouchableOpacity
-                                    style={styles.audioButton}
-                                    onPress={() => this.playSound()}
-                                >
-                                    <Text style={styles.audioButtonText}>Play Sound</Text>
-                                </TouchableOpacity>
-                            )}
-                        </View>
-                    </ScrollView>
-                </View>
-            </SafeAreaView>
-        );
+      const paragraph = this.props.currentGame.texte;
+      const etapeMax = this.props.parcoursInfo.etape_max;
+      if (etapeMax === undefined) {
+          var topBarreName = "";
+      } else {
+          var topBarreName = "Étape : " + this.props.currentGame.n_etape + "/" + etapeMax;
+      }
+      const title = this.props.currentGame.nom;
+      const icone = require('./../../../assets/code_paysage_icone.png');
+      const illustration = this.props.currentGame.image_url;
+      return (
+        <SafeAreaView style={styles.outsideSafeArea}>
+          <TopBarre name={topBarreName} />
+          <View style={styles.globalContainer}>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer} style={styles.scrollView}>
+              <View style={styles.card}>
+                <MainTitle title={title} icone={icone} />
+                {(illustration != '') && (<Image source={{ uri: illustration }} style={styles.areaImage} />)}
+                <Text style={styles.description}>{paragraph}</Text>
+                <TextInput style={styles.inputTextField} onChangeText={this.handleChange} editable={true} placeholder="CODE" />
+              </View>
+              <View style={styles.rightAlign}>
+                <TouchableOpacity
+		  style={styles.bouton}
+                  disabled={this.state.confirmClicked}
+                  onPress={() => {
+                    if (NormalizeStrings(this.state.code) == NormalizeStrings(this.state.input)) {
+                      this.handleConfirmClicked();
+                      this.props.navigation.navigate("GamePage", { parcoursInfo: this.props.parcoursInfo, parcours: this.props.parcours });
+                    }
+                    else {
+                      Alert.alert("Mauvais code !");
+                    }
+                  }}>
+                  <Text style={styles.boutonText}> {"Valider"} </Text>
+                </TouchableOpacity>
+                {this.state.isSoundLoaded && (
+                   <TouchableOpacity
+                     style={styles.audioButton}
+                     onPress={() => this.playSound()}
+                   >
+                     <Text style={styles.audioButtonText}>Play Sound</Text>
+                   </TouchableOpacity>)}
+              </View>
+            </ScrollView>
+          </View>
+        </SafeAreaView>
+      );
     }
 }
 
